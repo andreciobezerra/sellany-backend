6316162e774851e031aa2aeee4c08d375c318702
andrecio <andreciobezerra@gmail.com>
2021-02-14 23:54:10 -0300

    refactoring to use of Router

--- tests/functional/product.test.ts
+++ tests/functional/product.test.ts
@@ -3,7 +3,7 @@ describe("Product CRUD operations tests", () => {
     const { body, status } = await global.testRequest.post("/product/create-product");
 
     expect(status).toEqual(200);
-    expect(body).toEqual({ message: "Produto criado." });
+    expect(body).toEqual({ message: "Product created with success." });
   });
 
   test("Get a product", async (): Promise<void> => {
