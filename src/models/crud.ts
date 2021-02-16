aa3b9de7e8d444419d3b9ea8c4ba2467c47f1180
andrecio <andreciobezerra@gmail.com>
2021-02-16 00:19:22 -0300

    add IDBDrive

--- src/models/crud.ts
+++ src/models/crud.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import IService from "@src/interfaces-types/service-interface";
 import ICrudMessages from "@src/interfaces-types/crud-messages-interface";
+import IDBDrive from "@src/interfaces-types/db-drive-interface";
 
 abstract class Crud implements IService {
   private static Messages: ICrudMessages;
+  private static DBDrive: IDBDrive;
 
-  constructor(crudMessages: ICrudMessages) {
+  constructor(crudMessages: ICrudMessages, dbDrive: IDBDrive) {
     Crud.Messages = crudMessages;
+    Crud.DBDrive = dbDrive;
   }
 
   create(req: Request, res: Response): void {
