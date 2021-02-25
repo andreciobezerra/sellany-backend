4b97c9d2262695465aa47b8771127139956dfbaa
andrecio <andreciobezerra@gmail.com>
2021-02-25 20:37:44 -0300

    add new types

--- src/interfaces-types/types.ts
+++ src/interfaces-types/types.ts
@@ -1,9 +1,15 @@
-import Company from "@src/models/entities/company";
-import Owner from "@src/models/entities/owner";
-import Product from "@src/models/entities/product";
+import type Client from "@src/models/entities/client";
+import type Company from "@src/models/entities/company";
+import type Order from "@src/models/entities/order";
+import type Owner from "@src/models/entities/owner";
+import type Product from "@src/models/entities/product";
+import type Salesman from "@src/models/entities/salesman";
 
 export type int = number & { __int__: void };
 
-export type Entity = Product | Company | Owner;
+export type Entity = Product | Company | Owner | Order | Client | Salesman;
 
-export type Data = Record<string, string | number | Array<string> | Entity | Array<Owner>>;
+export type Data = Record<
+  string,
+  string | number | Array<string> | Entity | Array<Owner> | Array<Product>
+>;
