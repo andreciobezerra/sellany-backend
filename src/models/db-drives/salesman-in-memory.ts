b4a1183794528c38c13d3b68a879abfffd9f2710
andrecio <andreciobezerra@gmail.com>
2021-02-26 20:05:55 -0300

    create

---
+++ src/models/db-drives/salesman-in-memory.ts
@@ -0,0 +1,42 @@
+import IDBStrategy from "@src/interfaces-types/db-strategy-interface";
+import type { Data } from "@src/interfaces-types/types";
+import type Salesman from "../entities/salesman";
+
+class SalesmanInMemory implements IDBStrategy {
+  readonly data: Array<Data> = [];
+
+  create(elem: Salesman): void {
+    this.data.push({ id: `${this.data.length + 1}`, ...elem });
+  }
+  read(id: string): Salesman {
+    const company = this.data.find((company) => company.id === id);
+
+    return (company as unknown) as Salesman;
+  }
+
+  readAll(): Array<Salesman> {
+    return (this.data as unknown) as Array<Salesman>;
+  }
+
+  update(id: string, newData: Data): void {
+    const salesmanIndex = this.data.findIndex((sm) => sm.id === id);
+
+    if (salesmanIndex > -1) {
+      const salesman = this.data[salesmanIndex];
+
+      const salesmanUpdate = { ...salesman, ...newData };
+
+      this.data.splice(salesmanIndex, 1, salesmanUpdate);
+    }
+  }
+
+  delete(id: string): void {
+    const salesmanIndex = this.data.findIndex((sm) => sm.id === id);
+
+    if (salesmanIndex > -1) {
+      this.data.splice(salesmanIndex, 1);
+    }
+  }
+}
+
+export default SalesmanInMemory;
